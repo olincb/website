9c9284392748e7f4e62be39eec26c535f8875318
chris olin <bailson.jr@gmail.com>
2022-03-14 18:34:07 -0400

    lots of styling, adding pages other than home page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,4 @@
-class ProjectsController < ApplicationController
+class ProjectsController < PagesController
   def index
     @projects = Project.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
+  get '/about', to: 'pages#about'
+  get '/resume', to: 'pages#resume'
+  get '/projects', to: 'projects#index'
+  get '/art', to: 'art#index'
+
   resources :projects
 end
