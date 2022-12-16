294049c9789449d18a5b0152b99e318272f6a216
chris olin <bailson.jr@gmail.com>
2022-12-16 18:01:25 -0500

    Add project page for Smart Lights Controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,13 @@ class PagesController < ApplicationController
 
         @projects = Dir["#{Rails.root}/app/views/markdown/projects/*.md"]
         @projects.map! { |filepath| filepath.split('_')[-1].chop.chop.chop } # chop chop chop!!! (get rid of ".md")
+        # move the projects I want to highlight to the top of the list
+        @projects.insert(0, @projects.delete('This website'))
+        @projects.insert(0, @projects.delete('Smart Lights Controller'))
+        @projects.insert(0, @projects.delete('Wacket'))
+        # and less importand projects to the bottom of the list
+        @projects.insert(-1, @projects.delete('CAD Lock'))
+        # ... otherwise, the order is unimportant
         puts @projects
     end
       
